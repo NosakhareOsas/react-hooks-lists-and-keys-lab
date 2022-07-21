09fb517703a0fedd7e0700db3df3b2c79e0636d2
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-07-21 07:39:00 +0100

    Imported nanoid library to manage id

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { nanoid } from 'nanoid';
+
+
 
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (<nav>
+    {links.map((link) => <a href={'#'+link} key = {nanoid(5)}>{link}</a>)}
+    </nav>);
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { nanoid } from 'nanoid';
 
 function ProjectItem({ name, about, technologies }) {
   return (
@@ -7,6 +8,7 @@ function ProjectItem({ name, about, technologies }) {
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((technology) => <span key = {nanoid(5)}>{technology}</span>)}
       </div>
     </div>
   );
